24f621cba1ee7597682639c884787d1c28de7038
Hanin Farhan <haninfarhan96@gmail.com>
2024-03-23 10:56:43 +0100

    skapat html fil och städat upp filer

--- main.js
+++ main.js
@@ -1,2 +1,15 @@
 //Typescript kod
-console.log("Test");
+function courses(courseInfo) {
+    console.log(courseInfo.code);
+    console.log(courseInfo.name);
+    console.log(courseInfo.progression);
+    console.log(courseInfo.syllabus);
+}
+//test objekt att sätta in i funktion
+var newCourse = {
+    code: "dt210g",
+    name: "webbteknik",
+    progression: "A",
+    syllabus: "url-länk"
+};
+courses(newCourse);
--- main.ts
+++ main.ts
@@ -1,2 +1,26 @@
 //Typescript kod
-console.log("Test")
+
+//skapar interface som innehåller info om kurs
+interface CourseInfo {
+    code : string;
+    name : string;
+    progression : string;
+    syllabus : string
+}
+
+function courses(courseInfo: CourseInfo) : void {
+    console.log(courseInfo.code);
+    console.log(courseInfo.name);
+    console.log(courseInfo.progression);
+    console.log(courseInfo.syllabus);
+}
+
+//test objekt att sätta in i funktion
+const newCourse: CourseInfo = {
+    code: "dt210g",
+    name: "webbteknik",
+    progression:"A",
+    syllabus: "url-länk"
+};
+
+courses(newCourse);
